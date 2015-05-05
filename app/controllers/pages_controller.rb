d38f37a438f120c6f9361f6b20b060bf8ed8fa2a
sharonwarner <sharon.warner56@gmail.com>
2015-05-04 22:14:40 -0400

    Initial Commit!

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def weddingparty
+  end
+
+  def eventdetails
+  end
+
+  def rsvp
+  end
+
+  def registry
+  end
+
+end
